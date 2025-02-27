2289b72685c7ec308e72dfd6ce9d59b703ed38b0
BarisPozlu <131907140+BarisPozlu@users.noreply.github.com>
2025-02-27 16:38:11 +0300

    Face culling and small refactor

--- Lypant/src/Lypant/Renderer/OpenGL/RendererAPI_OpenGL.cpp
+++ Lypant/src/Lypant/Renderer/OpenGL/RendererAPI_OpenGL.cpp
@@ -10,9 +10,9 @@ namespace lypant
 	void RendererAPI::Init()
 	{
 		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glBlendEquation(GL_FUNC_ADD);
 	}
 
 	void RendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
